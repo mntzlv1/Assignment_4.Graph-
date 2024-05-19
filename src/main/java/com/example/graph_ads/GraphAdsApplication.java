903c1222f1310d32e8f6fa2cda06e35f0f518ba5
Amina <231599@astanait.edu.kz>
2024-05-19 18:24:54 +0600

    first in it

---
+++ src/main/java/com/example/graph_ads/GraphAdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.graph_ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphAdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphAdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graph_ads/GraphAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graph_ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphAdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
